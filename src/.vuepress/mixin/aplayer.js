8c6bd37b4a3364028b1040db3edd0defee68ca56
yuchanns <airamusume@gmail.com>
2019-07-31 23:55:03 +0800

    add bgm Cyberangel

--- src/.vuepress/mixin/aplayer.js
+++ src/.vuepress/mixin/aplayer.js
@@ -6,19 +6,21 @@ module.exports = {
     footer.appendChild(bsz)
     const APlayer = require('aplayer')
     // eslint-disable-next-line no-unused-vars
-    const ap = new APlayer({
-      container: document.getElementById('aplayer'),
-      lrcType: 3,
-      audio: [{
-        name: 'Cyberangel',
-        artist: 'Hanser',
-        url: 'https://www.yuchanns.xyz/assets/music/Cyberangle.flac',
-        cover: 'https://avatars3.githubusercontent.com/u/25029451',
-        lrc: 'https://www.yuchanns.xyz/assets/music/Cyberangle.lrc',
-      }],
-      mini: true,
-      fixed: true,
-      autoplay: true,
-    })
+    setTimeout(function () {
+      const ap = new APlayer({
+        container: document.getElementById('aplayer'),
+        lrcType: 3,
+        audio: [{
+          name: 'Cyberangel',
+          artist: 'Hanser',
+          url: 'https://www.yuchanns.xyz/assets/music/Cyberangle.flac',
+          cover: 'https://avatars3.githubusercontent.com/u/25029451',
+          lrc: 'https://www.yuchanns.xyz/assets/music/Cyberangle.lrc',
+        }],
+        mini: true,
+        fixed: true,
+        autoplay: true,
+      })
+    }, 1000)
   },
 }
