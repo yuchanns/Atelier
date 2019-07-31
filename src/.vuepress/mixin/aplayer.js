c767db13dfd2527e58ee37bedc70d3d382f4e586
yuchanns <airamusume@gmail.com>
2019-08-01 00:53:47 +0800

    add bgm

--- src/.vuepress/mixin/aplayer.js
+++ src/.vuepress/mixin/aplayer.js
@@ -9,13 +9,41 @@ module.exports = {
     const ap = new APlayer({
       container: document.getElementById('aplayer'),
       lrcType: 3,
-      audio: [{
-        name: 'Cyberangel',
-        artist: 'Hanser',
-        url: 'https://www.yuchanns.xyz/assets/music/Cyberangle.flac',
-        cover: 'https://avatars3.githubusercontent.com/u/25029451',
-        lrc: 'https://www.yuchanns.xyz/assets/music/Cyberangle.lrc',
-      }],
+      audio: [
+        {
+          name: 'Cyberangel',
+          artist: 'Hanser',
+          url: '/assets/music/Cyberangle.flac',
+          cover: '/assets/music/Cyberangle.gif',
+          lrc: '/assets/music/Cyberangle.lrc',
+        },
+        {
+          name: 'Gray Zone',
+          artist: 'Stack',
+          url: '/assets/music/GrayZone.flac',
+          cover: '/assets/music/GrayZone.jpg',
+          lrc: '/assets/music/GrayZone.lrc',
+        },
+        {
+          name: 'Bon☆Dance',
+          artist: 'Stack',
+          url: '/assets/music/Bon☆Dance.flac',
+          cover: '/assets/music/Bon☆Dance.jpg',
+          lrc: '/assets/music/Bon☆Dance.lrc',
+        },
+        {
+          name: 'Starlight Dance Floor',
+          artist: 'Ark Brown',
+          url: '/assets/music/StarlightDanceFloor.mp3',
+          cover: '/assets/music/StarlightDanceFloor.jpg',
+        },
+        {
+          name: 'Girls in the Mirror',
+          artist: 'Ark Brown',
+          url: '/assets/music/GirlsintheMirror.flac',
+          cover: '/assets/music/GirlsintheMirror.jpg',
+        },
+      ],
       fixed: true,
     })
     setTimeout(function () {
