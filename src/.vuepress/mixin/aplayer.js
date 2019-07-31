3f933d8e0bba6b088e01ec2f9137b0aa26f8328e
yuchanns <airamusume@gmail.com>
2019-08-01 00:10:01 +0800

    chore: make aplayer playing in 3 seconds

--- src/.vuepress/mixin/aplayer.js
+++ src/.vuepress/mixin/aplayer.js
@@ -1,26 +1,25 @@
 module.exports = {
   mounted () {
-    const bsz = document.createElement('div')
-    bsz.setAttribute('id', 'aplayer')
+    const aplayer = document.createElement('div')
+    aplayer.setAttribute('id', 'aplayer')
     const footer = document.getElementsByTagName('footer')[0]
-    footer.appendChild(bsz)
+    footer.appendChild(aplayer)
     const APlayer = require('aplayer')
     // eslint-disable-next-line no-unused-vars
+    const ap = new APlayer({
+      container: document.getElementById('aplayer'),
+      lrcType: 3,
+      audio: [{
+        name: 'Cyberangel',
+        artist: 'Hanser',
+        url: 'https://www.yuchanns.xyz/assets/music/Cyberangle.flac',
+        cover: 'https://avatars3.githubusercontent.com/u/25029451',
+        lrc: 'https://www.yuchanns.xyz/assets/music/Cyberangle.lrc',
+      }],
+      fixed: true,
+    })
     setTimeout(function () {
-      const ap = new APlayer({
-        container: document.getElementById('aplayer'),
-        lrcType: 3,
-        audio: [{
-          name: 'Cyberangel',
-          artist: 'Hanser',
-          url: 'https://www.yuchanns.xyz/assets/music/Cyberangle.flac',
-          cover: 'https://avatars3.githubusercontent.com/u/25029451',
-          lrc: 'https://www.yuchanns.xyz/assets/music/Cyberangle.lrc',
-        }],
-        mini: true,
-        fixed: true,
-        autoplay: true,
-      })
-    }, 1000)
+      ap.play()
+    }, 3000)
   },
 }
